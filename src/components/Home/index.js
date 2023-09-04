bd941c7d17bce29f26a0c24879661224bd486489
sam khan <samk.code@gmail.com>
2023-09-05 02:14:32 +0500

    --update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -65,7 +65,7 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>JavaScript Expert / RESTful APIs / Cloud Enthusiast</h2>
+          <h2>React / NodeJS / Express / Mongodb / JavaScript / TypeScript <br/> / Docker / Git / HTML / CSS </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
